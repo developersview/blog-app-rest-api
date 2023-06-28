9825ee28073dcf07f69acb205a0c7480a0791a80
developersview <pcslg1998@gmail.com>
2023-06-28 18:40:32 +0530

    comment service

---
+++ springboot-blog-app-rest-api/src/main/java/com/developersview/springbootblogapprestapi/service/impl/CommentServiceImpl.java
@@ -0,0 +1,60 @@
+package com.developersview.springbootblogapprestapi.service.impl;
+
+import com.developersview.springbootblogapprestapi.entity.Comment;
+import com.developersview.springbootblogapprestapi.entity.Post;
+import com.developersview.springbootblogapprestapi.exception.ResourceNotFoundException;
+import com.developersview.springbootblogapprestapi.payload.CommentDto;
+import com.developersview.springbootblogapprestapi.repository.CommentRepository;
+import com.developersview.springbootblogapprestapi.repository.PostRepository;
+import com.developersview.springbootblogapprestapi.service.CommentService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author pranoy.chakraborty
+ * @Date 28/06/2023
+ */
+@Service
+public class CommentServiceImpl implements CommentService {
+
+    private CommentRepository commentRepository;
+    private PostRepository postRepository;
+
+    public CommentServiceImpl(CommentRepository commentRepository, PostRepository postRepository) {
+        this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public CommentDto createComment(long postId, CommentDto commentDto) {
+        Comment comment = mapToEntity(commentDto);
+
+        //retrive post entity by id
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException("Post", "id", postId));
+
+        //set post to comment entity
+        comment.setPost(post);
+
+        //save comment
+        Comment newComment = commentRepository.save(comment);
+        return mapToDto(newComment);
+    }
+
+    private CommentDto mapToDto(Comment comment) {
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(comment.getId());
+        commentDto.setName(comment.getName());
+        commentDto.setEmail(comment.getEmail());
+        commentDto.setBody(comment.getBody());
+        return commentDto;
+    }
+
+    private Comment mapToEntity(CommentDto commentDto) {
+        Comment comment = new Comment();
+        comment.setId(commentDto.getId());
+        comment.setName(commentDto.getName());
+        comment.setEmail(commentDto.getEmail());
+        comment.setBody(commentDto.getBody());
+        return comment;
+    }
+}
