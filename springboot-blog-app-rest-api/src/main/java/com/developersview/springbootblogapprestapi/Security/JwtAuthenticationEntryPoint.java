a5d7204d47176d39d8a793202b724c61302e12e7
developersview <pcslg1998@gmail.com>
2023-07-14 02:01:38 +0530

    JwtAuthenticationEntryPoint

---
+++ springboot-blog-app-rest-api/src/main/java/com/developersview/springbootblogapprestapi/Security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,25 @@
+package com.developersview.springbootblogapprestapi.Security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * @author pranoy.chakraborty
+ * @Date 14/07/2023
+ */
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}
