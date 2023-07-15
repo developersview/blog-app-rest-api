031ae3b93bf85d9c0baeeca4b560ac7d8073c0f8
developersview <pcslg1998@gmail.com>
2023-07-16 00:01:36 +0530

    category entity

--- springboot-blog-app-rest-api/src/main/java/com/developersview/springbootblogapprestapi/entity/Category.java
+++ springboot-blog-app-rest-api/src/main/java/com/developersview/springbootblogapprestapi/entity/Category.java
@@ -22,7 +22,7 @@ public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
     private String name;
     private String description;
 
