794080fead32904795871a62ab7a5f85c01e231b
developersview <pcslg1998@gmail.com>
2023-07-15 23:45:07 +0530

    category entity and mapping

---
+++ springboot-blog-app-rest-api/src/main/java/com/developersview/springbootblogapprestapi/entity/Category.java
@@ -0,0 +1,31 @@
+package com.developersview.springbootblogapprestapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * @author pranoy.chakraborty
+ * @Date 15/07/2023
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String name;
+    private String description;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts;
+}
