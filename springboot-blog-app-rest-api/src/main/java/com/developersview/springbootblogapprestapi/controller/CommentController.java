e03d1b26dfa92789995006b6e0c99f6594c3e05b
developersview <pcslg1998@gmail.com>
2023-07-19 00:58:20 +0530

    comment customization

--- springboot-blog-app-rest-api/src/main/java/com/developersview/springbootblogapprestapi/controller/CommentController.java
+++ springboot-blog-app-rest-api/src/main/java/com/developersview/springbootblogapprestapi/controller/CommentController.java
@@ -2,6 +2,9 @@ package com.developersview.springbootblogapprestapi.controller;
 
 import com.developersview.springbootblogapprestapi.payload.CommentDto;
 import com.developersview.springbootblogapprestapi.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,9 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/api/")
+@Tag(
+        name = "CRUD REST API for Blog Comment Resources"
+)
 public class CommentController {
     private CommentService commentService;
 
@@ -22,6 +28,16 @@ public class CommentController {
         this.commentService = commentService;
     }
 
+
+    //create a comment
+    @Operation(
+            summary = "Create Comment Rest Endpoint",
+            description = "This endpoint is used to save a comment for a post into database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status - 201 CREATED"
+    )
     @PostMapping("/posts/{postId}/comments")
     public ResponseEntity<CommentDto> addComment(@PathVariable(value = "postId") long postId,
                                                  @Valid @RequestBody CommentDto commentDto) {
@@ -30,11 +46,29 @@ public class CommentController {
                 HttpStatus.CREATED);
     }
 
+    //get all comments
+    @Operation(
+            summary = "Get All Comments Rest Endpoint",
+            description = "This endpoint is used to get all comments for a post from database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status - 200 SUCCESS"
+    )
     @GetMapping("/posts/{postId}/comments")
     public List<CommentDto> getAllCommentsByPostId(@PathVariable(value = "postId") long postId) {
         return commentService.getAllCommentsByPostId(postId);
     }
 
+    //get a comment by id
+    @Operation(
+            summary = "Get a Comment by comment ID Rest Endpoint",
+            description = "This endpoint is used to get single comment for a post from database by comment ID"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status - 200 SUCCESS"
+    )
     @GetMapping("/posts/{postId}/comments/{commentId}")
     public ResponseEntity<CommentDto> getCommentById(@PathVariable(value = "postId") long postId,
                                                      @PathVariable(value = "commentId") long commentId) {
@@ -43,6 +77,15 @@ public class CommentController {
                 HttpStatus.OK);
     }
 
+    //update a comment by id
+    @Operation(
+            summary = "Update Comment Rest Endpoint",
+            description = "This endpoint is used to update a comment for a post into database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status - 200 SUCCESS"
+    )
     @PutMapping("/posts/{postId}/comments/{commentId}")
     public ResponseEntity<CommentDto> updateCommentById(@PathVariable(value = "postId") long postId,
                                                         @PathVariable(value = "commentId") long commentId,
@@ -52,6 +95,15 @@ public class CommentController {
                 HttpStatus.OK);
     }
 
+    //delete a comment by id
+    @Operation(
+            summary = "Delete Comment Rest Endpoint",
+            description = "This endpoint is used to delete a comment from a post from database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status - 200 SUCCESS"
+    )
     @DeleteMapping("/posts/{postId}/comments/{commentId}")
     public ResponseEntity<HttpStatus> deleteComment(@PathVariable(value = "postId") long postId,
                                                     @PathVariable(value = "commentId") long commentId){
