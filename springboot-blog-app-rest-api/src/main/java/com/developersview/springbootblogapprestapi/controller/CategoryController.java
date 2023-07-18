fc12cc46a1f328817eb2c48b86d4a648c2e866fb
developersview <pcslg1998@gmail.com>
2023-07-19 01:10:42 +0530

    category controller customization

--- springboot-blog-app-rest-api/src/main/java/com/developersview/springbootblogapprestapi/controller/CategoryController.java
+++ springboot-blog-app-rest-api/src/main/java/com/developersview/springbootblogapprestapi/controller/CategoryController.java
@@ -3,7 +3,10 @@ package com.developersview.springbootblogapprestapi.controller;
 import com.developersview.springbootblogapprestapi.entity.Category;
 import com.developersview.springbootblogapprestapi.payload.CategoryDto;
 import com.developersview.springbootblogapprestapi.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -17,6 +20,9 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("api/categories")
+@Tag(
+        name = "CRUD REST API for Category Resource"
+)
 public class CategoryController {
     private CategoryService categoryService;
 
@@ -24,6 +30,15 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    //create a category
+    @Operation(
+            summary = "Create Category Rest Endpoint",
+            description = "This endpoint is used to save a category into database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status - 201 CREATED"
+    )
     @SecurityRequirement(
             name = "Bear Authentication"
     )
@@ -34,17 +49,44 @@ public class CategoryController {
         return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
     }
 
+    //get a category by id
+    @Operation(
+            summary = "Get a Category Rest Endpoint",
+            description = "This endpoint is used to get a category from database by category ID"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status - 200 SUCCESS"
+    )
     @GetMapping("{id}")
     public ResponseEntity<CategoryDto> getCategory(@PathVariable("id") Long categoryId) {
         CategoryDto categoryDto = categoryService.getCategory(categoryId);
         return new ResponseEntity<>(categoryDto, HttpStatus.OK);
     }
 
+    //get all categories
+    @Operation(
+            summary = "Get All Categories Rest Endpoint",
+            description = "This endpoint is used to get all categories from database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status - 200 SUCCESS"
+    )
     @GetMapping
     public ResponseEntity<List<CategoryDto>> getCategories() {
         return ResponseEntity.ok(categoryService.getALlCategories());
     }
 
+    //update a category
+    @Operation(
+            summary = "Update Category Rest Endpoint",
+            description = "This endpoint is used to update a category into database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status - 200 SUCCESS"
+    )
     @SecurityRequirement(
             name = "Bear Authentication"
     )
@@ -56,6 +98,15 @@ public class CategoryController {
         return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
     }
 
+    //delete a category
+    @Operation(
+            summary = "Delete Category Rest Endpoint",
+            description = "This endpoint is used to delete a category from database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status - 200 SUCCESS"
+    )
     @SecurityRequirement(
             name = "Bear Authentication"
     )
